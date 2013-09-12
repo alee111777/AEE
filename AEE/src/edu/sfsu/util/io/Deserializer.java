4bbf16055ce5abb16a2404e977c184ca094c2139
eric <eric@eric-K54C>
2013-09-12 12:43:10 -0700

    util package created

--- AEE/src/edu/sfsu/util/io/Deserializer.java
+++ AEE/src/edu/sfsu/util/io/Deserializer.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.sfsu.io;
+package edu.sfsu.util.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
